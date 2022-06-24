14b39f9c2784e6996c07cac5d883039059b2bb15
2himitu <hmhnes@gmail.com>
2022-06-24 20:45:44 +0900

    exit까지 명령어 계속 받기

--- src/java/com/lhs/exam/board/main.java
+++ src/java/com/lhs/exam/board/main.java
@@ -7,8 +7,13 @@ public class main {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 계시판 v 0.1 ==" );
         System.out.println("== 프로그램 시작 ==" );
-        System.out.printf("명령) " );
-        System.out.println("입력된 명령어 :"+sc.nextLine());
+        String cmd;
+        do {
+            System.out.printf("명령) " );
+            cmd = sc.nextLine();
+            System.out.printf("입력된 명령어 : %s\n",cmd);
+        }
+        while(!cmd.equals("exit"));
         System.out.println("== 프로그램 종료 ==" );
         sc.close();
     }
