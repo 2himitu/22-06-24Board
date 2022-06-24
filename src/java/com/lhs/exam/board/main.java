3fc35e527730dfaf79cc7253f6c5f3b1c93c919d
2himitu <hmhnes@gmail.com>
2022-06-24 20:15:14 +0900

    시작 알림 메시지 출력

--- src/java/com/lhs/exam/board/main.java
+++ src/java/com/lhs/exam/board/main.java
@@ -2,6 +2,9 @@ package com.lhs.exam.board;
 
 public class main {
     public static void main(String[] args) {
-
+        System.out.println("== 계시판 v 0.1 ==" );
+        System.out.println("== 프로그램 시작 ==" );
+        System.out.println("명령" );
+        System.out.println("== 프로그램 종료 ==" );
     }
 }
