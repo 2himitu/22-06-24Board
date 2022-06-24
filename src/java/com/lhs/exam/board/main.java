c845dd1d6ae298d6739f4c3fbd9f11ee8898e1cd
2himitu <hmhnes@gmail.com>
2022-06-24 20:23:17 +0900

    명령어 입력

--- src/java/com/lhs/exam/board/main.java
+++ src/java/com/lhs/exam/board/main.java
@@ -1,10 +1,15 @@
 package com.lhs.exam.board;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("== 계시판 v 0.1 ==" );
         System.out.println("== 프로그램 시작 ==" );
-        System.out.println("명령" );
+        System.out.printf("명령) " );
+        System.out.println("입력된 명령어 :"+sc.nextLine());
         System.out.println("== 프로그램 종료 ==" );
+        sc.close();
     }
 }
